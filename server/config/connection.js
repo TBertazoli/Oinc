df676ca9caa427f758efcfcb1f353ed9c5ccd123
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2023-10-19 16:10:25 -0500

    added console.log

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Oink!', {
+console.log("process.env.MONGODB_URI", process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Oink!", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
